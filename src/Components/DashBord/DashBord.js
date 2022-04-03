17d631b0aba14128c3f1cc6025551f989844cedf
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-04-04 04:06:21 +0600

    this is first chart in this page dashbord chekout

--- src/Components/DashBord/DashBord.js
+++ src/Components/DashBord/DashBord.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import LineCharts from '../LineChart/LineChart';
 
 const DashBord = () => {
     return (
-        <div>
-            this is dashbord
-        </div>
+       <div className='grid md:grid-cols-2 sm:grid-cols-1 p-8 m-8 border-2 rounded-lg'>
+           <div className='w-full border-2 border-red-500 p-4'>
+           <LineCharts></LineCharts>
+           </div>
+           <div>good job</div>
+       </div>
     );
 };
 
---
+++ src/Components/LineChart/LineChart.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+} from "recharts";
+const data = [
+  {
+    id: 1,
+    month: "Mar",
+    investment: 100000,
+    sell: 241,
+    revenue: 10401,
+  },
+  {
+    id: 2,
+    month: "Apr",
+    investment: 200000,
+    sell: 423,
+    revenue: 24500,
+  },
+  {
+    id: 3,
+    month: "May",
+    investment: 500000,
+    sell: 726,
+    revenue: 67010,
+  },
+  {
+    id: 4,
+    month: "Jun",
+    investment: 500000,
+    sell: 529,
+    revenue: 40405,
+  },
+  {
+    id: 5,
+    month: "Jul",
+    investment: 600000,
+    sell: 601,
+    revenue: 50900,
+  },
+  {
+    id: 6,
+    month: "Aug",
+    investment: 700000,
+    sell: 670,
+    revenue: 61000,
+  },
+];
+
+const LineCharts = () => {
+  return (
+    <>
+      <h1 className="chart-heading text-red-500 text-2xl">Line Chart</h1>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="month" interval={"preserveStartEnd"} />
+          <YAxis />
+          <Tooltip/>
+          <Legend />
+          <Line dataKey="revenue" stroke="red" activeDot={{r:8}}/>
+          <Line dataKey="id" stroke="green"  activeDot={{r:8}}/>
+          <Line dataKey="investment" stroke="blue"  activeDot={{r:8}}/>
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
+
+export default LineCharts;

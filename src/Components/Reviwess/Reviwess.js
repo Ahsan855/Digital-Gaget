b806fdf864f47a614e586ff12596d70b6935f7ce
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-04-04 17:50:45 +0600

    done all file and image adding for me

--- src/Components/Reviwess/Reviwess.js
+++ src/Components/Reviwess/Reviwess.js
@@ -4,6 +4,7 @@ import useReviwes from "../Reviwes/useReviwes";
 const Reviwess = ({ reviwePd }) => {
   return (
     <div className="grid grid-cols-1 gap-4 p-6 m-8 border-4 rounded-xl mb-10 bg-white">
+        <img className="w-3/6 h-auto rounded-3xl mx-auto" src={reviwePd.img} alt="" />
       <h1 className="text-purple-500 text-2xl font-sans font-bold">Name: {reviwePd.name}</h1>
       <p className="text-xl w-full flex-wrap">{reviwePd.description}</p>
       <h2 className="text-3xl text-red-600">Ratting: {reviwePd.rating}</h2>

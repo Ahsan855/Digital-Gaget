40a618dacd94b04aadcb03e23e9710e4169ab965
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-04-04 16:10:35 +0600

    Reader chart done alhamdulillah

--- src/Components/DashBord/DashBord.js
+++ src/Components/DashBord/DashBord.js
@@ -1,20 +1,24 @@
 import AreaCharts from "../AreaChart/AreaChart";
 import BarCharts from "../BarChart/BarChart";
 import LineCharts from "../LineChart/LineChart";
+import ReaderChart from "../RedarChart/RedarChart";
 
 const DashBord = () => {
   return (
-      <div className="grid md:grid-cols-3 sm:grid-cols-1 p-44 m-8 border-2 rounded-lg">
-        <div className="w-full border-2 border-red-500 p-4">
-          <LineCharts></LineCharts>
-        </div>
-        <div>
-          <AreaCharts></AreaCharts>
-        </div>
-        <div>
-        <BarCharts></BarCharts>
-        </div>
+    <div className="grid grid-rows-2 gap-4 m-4 grid-cols-2 border-2 rounded-lg">
+      <div className="w-full mx-auto m-4 p-2">
+        <LineCharts></LineCharts>
       </div>
+      <div className="w-full mx-auto m-4 p-2">
+        <AreaCharts></AreaCharts>
+      </div>
+      <div className="w-full mx-auto m-4 p-2">
+        <BarCharts></BarCharts>
+      </div>
+      <div className="w-full mx-auto m-4 p-2">
+        <ReaderChart></ReaderChart>
+      </div>
+    </div>
   );
 };
 
--- src/Components/Reviwe/Reviwe.js
+++ src/Components/Reviwe/Reviwe.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Reviwe = () => {
+  
   return (<div>
     <h1> this is good emamph</h1>
   </div>);

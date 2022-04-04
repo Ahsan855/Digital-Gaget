fad245c38754a0e22f7872598235c07865ec8b0a
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-04-05 02:56:38 +0600

    done all destucturing done

--- src/Components/Reviwess/Reviwess.js
+++ src/Components/Reviwess/Reviwess.js
@@ -1,18 +1,15 @@
 import React from "react";
 
 const Reviwess = ({ reviwePd }) => {
+  const { name, img, rating, description } = reviwePd;
   return (
     <div className="grid grid-cols-1 gap-4 p-6 m-8 border-4 rounded-xl mb-10 bg-white">
-      <img
-        className="w-3/6 h-auto rounded-3xl mx-auto"
-        src={reviwePd.img}
-        alt=""
-      />
+      <img className="w-3/6 h-auto rounded-3xl mx-auto" src={img} alt="" />
       <h1 className="text-purple-500 text-2xl font-sans font-bold">
-        Name: {reviwePd.name}
+        Name: {name}
       </h1>
-      <p className="text-xl w-full flex-wrap">{reviwePd.description}</p>
-      <h2 className="text-3xl text-red-600">Rating: {reviwePd.rating}</h2>
+      <p className="text-xl w-full flex-wrap">{description}</p>
+      <h2 className="text-3xl text-red-600">Rating: {rating}</h2>
     </div>
   );
 };

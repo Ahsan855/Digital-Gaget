03fb01a7a9231b048391f1f31eafa472f2c52cac
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-04-04 13:58:10 +0600

    contact page done alhandulillah

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-            contact
+  return (
+    <div className="border p-44 bg-blue-400 w-full ">
+      <div className="flex flex-col md:flex-row justify-between items-center">
+        <div className="text-white text-2xl">
+          <h1>Street: 17/4 Shibganj Bogura</h1>
+          <h1>City: Bogura</h1>
+          <h1>Phone: +8801744154906</h1>
+          <h1>Email: muhammadrifathasan9869@gmail.com</h1>
         </div>
-    );
+        <div>
+          <h1 className="text-4xl text-white font-bold">Font-End Developer</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
